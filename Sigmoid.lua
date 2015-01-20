9c386cee4227baaa56c6e50857aa3fcb0eda1bdb
Sergey Zagoruyko <sergey.zagoruyko@imagine.enpc.fr>
2015-01-20 15:15:03 +0100

    backward compatibility of pointwise activations

--- Sigmoid.lua
+++ Sigmoid.lua
@@ -4,3 +4,8 @@ function Sigmoid:__init(inplace)
    parent.__init(self, inplace)
    self.mode = 'CUDNN_ACTIVATION_SIGMOID'
 end
+
+function Sigmoid:updateOutput(input)
+  if not self.mode then self.mode = 'CUDNN_ACTIVATION_SIGMOID' end
+  return parent.updateOutput(self, input)
+end
