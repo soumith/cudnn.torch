8021a68c445969d3afe7c03f6711a49d4454f1bf
Sergey Zagoruyko <zagoruyko2@gmail.com>
2015-12-24 14:40:21 +0100

    make avg-pooling swappable with nn

--- SpatialAveragePooling.lua
+++ SpatialAveragePooling.lua
@@ -1,9 +1,31 @@
 local SpatialAveragePooling, parent
    = torch.class('cudnn.SpatialAveragePooling', 'cudnn._Pooling')
 
-function SpatialAveragePooling:__init(kW, kH, dW, dH, padW, padH)
-   parent.__init(self, kW, kH, dW, dH, padW, padH)
-   self.mode = 'CUDNN_POOLING_AVERAGE_COUNT_INCLUDE_PADDING'
+local function backwardCompatible(self)
+   if self.ceil_mode == nil then
+      self.ceil_mode = false
+      self.count_include_pad = true
+      self.padH = 0
+      self.padW = 0
+   end
+end
+
+function SpatialAveragePooling:updateOutput(input)
+   -- for nn <> cudnn conversion
+   backwardCompatible(self)
+   if self.divide ~= nil then
+      assert(self.divide, 'not supported')
+   end
+
+   self.count_include_pad = self.count_include_pad ~= nil and 
+      self.count_include_pad or true
+   if self.count_include_pad then
+      self.mode = 'CUDNN_POOLING_AVERAGE_COUNT_INCLUDE_PADDING'
+   else 
+      error'This mode is untested in cudnn'
+      self.mode = 'CUDNN_POOLING_AVERAGE_COUNT_EXCLUDE_PADDING'
+   end
+   return parent.updateOutput(self, input)
 end
 
 function SpatialAveragePooling:__tostring__()
