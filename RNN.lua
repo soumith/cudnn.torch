cae779ed9593a86929a1f58f6c500d6aaa234514
Anthony Sandrin <asandrin@nvidia.com>
2016-03-16 18:12:02 -0700

    Don't save hidden states.
    Instead, use {hidden,cell}{Input,Output} and
    gradient{Hidden,Cell}{Input,Output} properties.

--- RNN.lua
+++ RNN.lua
@@ -7,10 +7,7 @@ function RNN:__init(hiddenSize, numLayers)
 
     self.datatype = 0 -- TODO CUDNN_FLOAT, should get the constant from ffi
     self.hiddenSize = hiddenSize
-    self.inputSize = 0
-    self.seqLength = 0
     self.numLayers = numLayers
-    self.miniBatch = 0
     self.bidirectional = 0
     self.inputMode = 0 -- TODO CUDNN_LINEAR_INPUT, should get the constant from ffi
     self.mode = 0 -- TODO CUDNN_RNN_RELU, should get the constant from ffi
@@ -21,11 +18,11 @@ function RNN:__init(hiddenSize, numLayers)
     self.output = torch.CudaTensor()
     self.weight = torch.CudaTensor()
     self.gradWeight = torch.CudaTensor()
-    self.hx = torch.CudaTensor()
-    self.cx = torch.CudaTensor()
-    self.hy = torch.CudaTensor()
-    self.cy = torch.CudaTensor()
     self.reserve = torch.CudaTensor(1)
+    self.hiddenOutput = torch.CudaTensor()
+    self.cellOutput = torch.CudaTensor()
+    self.gradHiddenInput = torch.CudaTensor()
+    self.gradCellInput = torch.CudaTensor()
 
     self:training()
 end
@@ -156,18 +153,17 @@ end
 
 function RNN:resetHiddenDescriptors()
     self.hxDesc = createTensorDescriptors(1)
-    local dim = torch.IntTensor({self.hx:size(3), self.hx:size(2), self.hx:size(1)})
+    self.hyDesc = createTensorDescriptors(1)
+
+    local dim = torch.IntTensor({self.hiddenSize, self.miniBatch, self.numLayers})
     local stride = torch.IntTensor({1, dim[1], dim[1] * dim[2]})
+
     errcheck('cudnnSetTensorNdDescriptor',
              self.hxDesc[0],
              self.datatype,
              3,
              dim:data(),
              stride:data())
-
-    self.hyDesc = createTensorDescriptors(1)
-    dim = torch.IntTensor({self.hy:size(3), self.hy:size(2), self.hy:size(1)})
-    stride = torch.IntTensor({1, dim[1], dim[1] * dim[2]})
     errcheck('cudnnSetTensorNdDescriptor',
              self.hyDesc[0],
              self.datatype,
@@ -178,18 +174,17 @@ end
 
 function RNN:resetCellDescriptors()
     self.cxDesc = createTensorDescriptors(1)
-    local dim = torch.IntTensor({self.cx:size(3), self.cx:size(2), self.cx:size(1)})
+    self.cyDesc = createTensorDescriptors(1)
+
+    local dim = torch.IntTensor({self.hiddenSize, self.miniBatch, self.numLayers})
     local stride = torch.IntTensor({1, dim[1], dim[1] * dim[2]})
+
     errcheck('cudnnSetTensorNdDescriptor',
              self.cxDesc[0],
              self.datatype,
              3,
              dim:data(),
              stride:data())
-
-    self.cyDesc = createTensorDescriptors(1)
-    dim = torch.IntTensor({self.cy:size(3), self.cy:size(2), self.cy:size(1)})
-    stride = torch.IntTensor({1, dim[1], dim[1] * dim[2]})
     errcheck('cudnnSetTensorNdDescriptor',
              self.cyDesc[0],
              self.datatype,
@@ -214,7 +209,7 @@ end
 
 function RNN:updateOutput(input)
 
-    assert(input:dim() == 3)
+    assert(input:dim() == 3, "Input should have three dimensions: (seqLength, miniBatch, inputSize)")
 
     -- Decide which descriptors/tensors need to be updated.
     local resetRNN = not DropoutDesc or not RNNDesc
@@ -234,15 +229,13 @@ function RNN:updateOutput(input)
         resetRNN = true
         resetIO = true
         resetHC = true
-        resetWeight = true
     end
 
-    if input:size(3) ~= self.inputSize then
-        self.inputSize = input:size(3)
-        resetRNN = true
-        resetIO = true
-        resetWeight = true
+    if not self.inputSize then
+      self.inputSize = input:size(3)
     end
+    assert(resetWeight or input:size(3) == self.inputSize,
+          "Input size has changed! clearState() must be called to resize weights.")
 
     -- Update descriptors/tensors
     if resetRNN then
@@ -251,22 +244,32 @@ function RNN:updateOutput(input)
     end
 
     local x = makeContiguous(self, input)
-    local y = self.output
+    local y = self.output:resize(self.seqLength, self.miniBatch, self.hiddenSize)
     if resetIO then
-        self.output:resize(self.seqLength, self.miniBatch, self.hiddenSize):zero()
         self:resetIODescriptors(input)
     end
 
-    -- Hidden/cell output becomes the new hidden/cell input.
-    local hx = self.hy
-    local cx = self.cy
-    local hy = self.hx
-    local cy = self.cx
+    -- Optionally use hiddenInput/cellInput parameters
+    local hx = self.hiddenInput
+    if hx then
+      assert(hx:dim() == 3, "Hidden input must have 3 dimensions: (numLayers, miniBatch, hiddenSize)")
+      assert(hx:size(1) == self.numLayers, "Hidden input has incorrect number of layers!")
+      assert(hx:size(2) == self.miniBatch, "Hidden input has incorrect number of minibathes!")
+      assert(hx:size(3) == self.hiddenSize, "Hidden input has incorrect size!")
+    end
+
+    local cx = self.cellInput
+    if cx then
+      assert(cx:dim() == 3, "Cell input must have 3 dimensions: (numLayers, miniBatch, hiddenSize)")
+      assert(cx:size(1) == self.numLayers, "Cell input has incorrect number of layers!")
+      assert(cx:size(2) == self.miniBatch, "Cell input has incorrect number of minibathes!")
+      assert(cx:size(3) == self.hiddenSize, "Cell input has incorrect size!")
+    end
+
+    local hy = self.hiddenOutput:resize(self.numLayers, self.miniBatch, self.hiddenSize)
+    local cy = self.cellOutput:resize(self.numLayers, self.miniBatch, self.hiddenSize)
+
     if resetHC then
-        self.hx:resize(self.numLayers, self.miniBatch, self.hiddenSize):zero()
-        self.cx:resize(self.numLayers, self.miniBatch, self.hiddenSize):zero()
-        self.hy:resize(self.numLayers, self.miniBatch, self.hiddenSize):zero()
-        self.cy:resize(self.numLayers, self.miniBatch, self.hiddenSize):zero()
         self:resetHiddenDescriptors()
         self:resetCellDescriptors()
     end
@@ -281,6 +284,7 @@ function RNN:updateOutput(input)
                  weightSize:data())
         weightSize[1] = (weightSize[1] + 3) / 4 -- sizeof(float)
         self.weight:resize(weightSize[1] / 4)
+        self.gradWeight:resizeAs(self.weight):zero()
         self:resetWeightDescriptors()
     end
 
@@ -312,8 +316,8 @@ function RNN:updateOutput(input)
                  cudnn.getHandle(),
                  self.rnnDesc[0],
                  self.xDescs, x:data(),
-                 self.hxDesc[0], hx:data(),
-                 self.cxDesc[0], cx:data(),
+                 self.hxDesc[0], hx and hx:data() or nil,
+                 self.cxDesc[0], cx and cx:data() or nil,
                  self.wDesc[0], w:data(),
                  self.yDescs, y:data(),
                  self.hyDesc[0], hy:data(),
@@ -332,65 +336,94 @@ function RNN:updateOutput(input)
                  self.hyDesc[0], hy:data(),
                  self.cyDesc[0], cy:data(),
                  self.workspace:data(), self.workspace:size(1) * 4) -- sizeof(float)
-
     end
 end
 
 function RNN:updateGradInput(input, gradOutput)
+    assert(input:dim() == 3, "Input should have three dimensions: (seqLength, miniBatch, inputSize)")
+    assert(input:size(1) == self.seqLength, "Sequence length has changed!")
+    assert(input:size(2) == self.miniBatch, "Minibatch size has changed!")
+    assert(input:size(3) == self.inputSize, "Input size has changed!")
 
-    assert(input:dim() == 3)
-    assert(input:size(1) == self.seqLength)
-    assert(input:size(2) == self.miniBatch)
-    assert(input:size(3) == self.inputSize)
-
-    assert(gradOutput:isSameSizeAs(self.output))
-    assert(self.train)
+    assert(gradOutput:isSameSizeAs(self.output), "gradOutput has incorrect size!")
+    assert(self.train, "updateGradInput can only be called when training!")
 
     local x, dy = makeContiguous(self, input, gradOutput)
     local y = self.output
     local w = self.weight
-    local hx = self.hx
-    local cx = self.cx
-    local dx = self.gradInput
+    local hx = self.hiddenInput
+    local dx = self.gradInput:resizeAs(input)
 
-    if not dx:isSameSizeAs(input) then
-        dx:resizeAs(input)
+    if hx then
+      assert(hx:dim() == 3, "Hidden input must have 3 dimensions: (numLayers, miniBatch, hiddenSize)")
+      assert(hx:size(1) == self.numLayers, "Hidden input has incorrect number of layers!")
+      assert(hx:size(2) == self.miniBatch, "Hidden input has incorrect number of minibathes!")
+      assert(hx:size(3) == self.hiddenSize, "Hidden input has incorrect size!")
     end
+
+    local cx = self.cellInput
+    if cx then
+      assert(cell:dim() == 3, "Cell input must have 3 dimensions: (numLayers, miniBatch, hiddenSize)")
+      assert(cell:size(1) == self.numLayers, "Cell input has incorrect number of layers!")
+      assert(cell:size(2) == self.miniBatch, "Cell input has incorrect number of minibathes!")
+      assert(cell:size(3) == self.hiddenSize, "Cell input has incorrect size!")
+    end
+
+    local dhy = self.gradHiddenOutput
+    if dhy then
+      assert(hx:dim() == 3, "Hidden output gradient must have 3 dimensions: (numLayers, miniBatch, hiddenSize)")
+      assert(hx:size(1) == self.numLayers, "Hidden output gradient has incorrect number of layers!")
+      assert(hx:size(2) == self.miniBatch, "Hidden output gradient has incorrect number of minibathes!")
+      assert(hx:size(3) == self.hiddenSize, "Hidden output gradient has incorrect size!")
+    end
+
+    local dcy = self.gradHiddenOutput
+    if dcy then
+      assert(cell:dim() == 3, "Cell output gradient must have 3 dimensions: (numLayers, miniBatch, hiddenSize)")
+      assert(cell:size(1) == self.numLayers, "Cell output gradient has incorrect number of layers!")
+      assert(cell:size(2) == self.miniBatch, "Cell output gradient has incorrect number of minibathes!")
+      assert(cell:size(3) == self.hiddenSize, "Cell output gradient has incorrect size!")
+    end
+
+    local dhx = self.gradHiddenInput:resize(self.numLayers, self.miniBatch, self.hiddenSize)
+    local dcx = self.gradCellInput:resize(self.numLayers, self.miniBatch, self.hiddenSize)
 
     errcheck('cudnnRNNBackwardData',
              cudnn.getHandle(),
              self.rnnDesc[0],
              self.yDescs, y:data(),
              self.yDescs, dy:data(),
-             self.hyDesc[0], nil, -- TODO should dhy be ignored?
-             self.cyDesc[0], nil, -- TODO should dhy be ignored?
+             self.hyDesc[0], dhy and dhy:data() or nil,
+             self.cyDesc[0], dcy and dcy:data() or nil,
              self.wDesc[0], w:data(),
-             self.hxDesc[0], hx:data(),
-             self.cxDesc[0], cx:data(),
+             self.hxDesc[0], hx and hx:data() or nil,
+             self.cxDesc[0], cx and cx:data() or nil,
              self.xDescs, dx:data(),
-             self.hxDesc[0], nil, -- TODO should dhx be ignored?
-             self.cxDesc[0], nil, -- TODO should dcx be ignored?
+             self.hxDesc[0], dhx:data(),
+             self.cxDesc[0], dcx:data(),
              self.workspace:data(), self.workspace:size(1) * 4, -- sizeof(float)
              self.reserve:data(), self.reserve:size(1) * 4) -- sizeof(float)
 end
 
 function RNN:accGradParameters(input, gradOutput, scale)
+    assert(input:dim() == 3, "Input should have three dimensions: (seqLength, miniBatch, inputSize)")
+    assert(input:size(1) == self.seqLength, "Sequence length has changed!")
+    assert(input:size(2) == self.miniBatch, "Minibatch size has changed!")
+    assert(input:size(3) == self.inputSize, "Input size has changed!")
 
-    assert(input:dim() == 3)
-    assert(input:size(1) == self.seqLength)
-    assert(input:size(2) == self.miniBatch)
-    assert(input:size(3) == self.inputSize)
-
-    assert(gradOutput:isSameSizeAs(self.output))
-    assert(self.train)
+    assert(gradOutput:isSameSizeAs(self.output), "gradOutput has incorrect size!")
+    assert(self.train, "updateGradInput can only be called when training!")
 
     local x, dy = makeContiguous(self, input, gradOutput)
-    local hx = self.hx
+    local hx = self.hiddenInput
     local y = self.output
     local dw = self.gradWeight
 
-    if not dw:isSameSizeAs(self.weight) then
-        dw:resizeAs(self.weight):zero()
+    if hx then
+      assert(hx:dim() == 3, "Hidden input must have 3 dimensions: (numLayers, miniBatch, hiddenSize)")
+      assert(hx:size(1) == self.numLayers, "Hidden input has incorrect number of layers!")
+      assert(hx:size(2) == self.miniBatch, "Hidden input has incorrect number of minibathes!")
+      assert(hx:size(3) == self.hiddenSize, "Hidden input has incorrect size!")
     end
 
     if scale == 0 then
@@ -399,7 +432,7 @@ function RNN:accGradParameters(input, gradOutput, scale)
 
     -- cudnnRNNBackwardWeights doesn't accept a scale parameter so instead
     -- scale before and after.
-    -- TODO: How much does this impact accuracy?
+    -- TODO: How much does this impact accuracy? Use a secondary buffer instead?
     if scale ~= 1 then
         local scaleTensor = torch.Tensor({1 / scale})
         errcheck('cudnnScaleTensor',
@@ -413,12 +446,11 @@ function RNN:accGradParameters(input, gradOutput, scale)
              cudnn.getHandle(),
              self.rnnDesc[0],
              self.xDescs, x:data(),
-             self.hxDesc[0], hx:data(),
+             self.hxDesc[0], hx and hx:data() or nil,
              self.yDescs, y:data(),
              self.workspace:data(), self.workspace:size(1) * 4, -- sizeof(float)
              self.wDesc[0], dw:data(),
              self.reserve:data(), self.reserve:size(1) * 4) -- sizeof(float)
-
 
     if scale ~= 1 then
         local scaleTensor = torch.Tensor({scale})
@@ -437,10 +469,10 @@ function RNN:clearDesc()
    self.wDesc = nil
    self.xDescs = nil
    self.yDescs = nil
-   self.hxDescs = nil
-   self.hyDescs = nil
-   self.cxDescs = nil
-   self.cyDescs = nil
+   self.hxDesc = nil
+   self.hyDesc = nil
+   self.cxDesc = nil
+   self.cyDesc = nil
 end
 
 function RNN:write(f)
@@ -454,7 +486,7 @@ end
 
 function RNN:clearState()
    self:clearDesc()
-   nn.utils.clear(self, '_input', '_gradOutput', 'hx', 'hy', 'cx', 'cy',
+   nn.utils.clear(self, '_input', '_gradOutput',
                         'reserve', 'dropoutStates')
    return parent.clearState(self)
 end
