501dc97f6720f448c8e4fc4fa70829230afe987e
soumith <soumith@gmail.com>
2016-03-17 15:11:17 -0400

    add _input and _gradOutput to clearState everywhere

--- VolumetricConvolution.lua
+++ VolumetricConvolution.lua
@@ -308,5 +308,7 @@ end
 function VolumetricConvolution:clearState()
    self:clearDesc()
    nn.utils.clear(self, 'extraBuffer')
+   self._gradOutput = nil
+   self._input = nil
    return nn.Module.clearState(self)
 end
